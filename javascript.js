e988497b2def1a32d35b27a92cc11ef0774a1fa5
amitpaul10 <amitpaul.bu@gmail.com>
2025-05-10 22:34:38 +0600

    learning js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,103 @@
+//!Day 1
+
+//todo Practics 1
 let age = 20;
 console.log(age);
-
 let bookToRead = 2;
 console.log(bookToRead);
-
 let wakeUpTime = 10;
 wakeUpTime = 6;
 console.log(wakeUpTime);
-
 let wastedTime = 6;
 console.log(wastedTime);
+let borrow = 0;
+console.log(borrow);
+let goals = 3;
+console.log(goals);
+let pillow = 1;
+console.log(pillow);
+
+//todo Practice 2
+
+let name = "Amit Kumar Paul";
+console.log(name);
+
+let address = "Satkhira";
+console.log(address);
+
+let bestFriendStudy = "Khulna University";
+console.log(bestFriendStudy);
+
+let fan = "Rashmika Mandana";
+console.log(fan);
+
+let movieOfTheYear = "Dil Bachara";
+console.log(movieOfTheYear);
+
+let hobby = "coding";
+console.log(hobby);
+
+let color = "Sky blue";
+console.log(color);
+
+//todo Practice 3
+
+let isPassed = false;
+console.log(isPassed);
+
+let massage = true;
+console.log(massage);
+
+let celebrating = true;
+console.log(celebrating);
+
+let isHappy = true;
+let hasMoney = false;
+let isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+let isStudyDone = false;
+console.log(isStudyDone);
+
+let isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+let isDinnerDone = false;
+console.log(isDinnerDone);
+
+//todo Practice 4
+
+let income = 100 - 40;
+console.log(income);
+
+let pencils = 10 / 2;
+console.log(pencils);
+
+let str1 = "hello";
+let str2 = "World";
+console.log(str1 + str2);
+
+let have = 60;
+console.log(have / 15);
+
+let chal = 153;
+let dal = 261;
+console.log(chal + dal);
+
+let haveMoney = 500;
+let cost = 75;
+console.log(haveMoney / cost);
+
+console.log(10 / 3);
+
+let first = 'Bangla';
+let second = 'desh';
+console.log(first + second);
+
+let haveChal = 480;
+let day = (haveChal / 4);
+console.log(day);
+console.log(day / 30);
+
+
+
