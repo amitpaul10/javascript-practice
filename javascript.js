a008a86e4964ffc5a298a44b253d304bb951ee70
amitpaul10 <amitpaul.bu@gmail.com>
2025-05-10 21:46:08 +0600

    Everyday practice

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let age = 20;
+console.log(age);
+
+let bookToRead = 2;
+console.log(bookToRead);
+
+let wakeUpTime = 10;
+wakeUpTime = 6;
+console.log(wakeUpTime);
+
+let wastedTime = 6;
+console.log(wastedTime);
